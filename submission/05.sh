f5bf6034ba293a0405d052685ce536e29f714b0f
muawiya1 <muawiyaabk@gmail.com>
2026-03-19 02:01:48 +0100

    Fix 03

--- submission/05.sh
+++ submission/05.sh
@@ -1,6 +1,6 @@
 # Check the total amount in the wallet.
 # Mine some blocks on regtest to fund the wallet in order to make it > 0 i.e to have balance
 WALLET_ADDR=$(bitcoin-cli -regtest -rpcwallet="builderswallet" getnewaddress)
-bitcoin-cli -regtest generatetoaddress 101 "$WALLET_ADD"
+bitcoin-cli -regtest generatetoaddress 101 "$WALLET_ADDR"
 
 bitcoin-cli -regtest -rpcwallet="builderswallet" getbalance
